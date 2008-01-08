6897c986b00021be0c3444cb095eeef2d326e1dd
Rafael Almeida <rafaelc@dcc.ufmg.br>
2008-01-07 22:36:46 -0200

    Added the setup.py file for installing the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gmailreader',
+      version='0.1',
+      description='E-mail reader for gmail',
+      author='Rafael C. Almeida',
+      author_email='almeidaraf@gmail.com',
+      url='http://www.dcc.ufmg.br/~rafaelc/programas.htm',
+      scripts=['gmailreader.py']
+     )
