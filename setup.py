8193184544076ea9dd81ef125219cde354afb144
Rafael Almeida <rafaelc@dcc.ufmg.br>
2008-02-06 14:50:27 -0200

    Setup script updated to install the manpage

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(name='gmailreader',
       author_email='almeidaraf@gmail.com',
       url='http://www.nongnu.org/gmailreader/',
       py_modules=['Config', 'configvars', 'MIMEParser', 'tabler'],
+      data_files=[('man/man1', ['gmailreader.1'])],
       scripts=['gmailreader.py']
      )
